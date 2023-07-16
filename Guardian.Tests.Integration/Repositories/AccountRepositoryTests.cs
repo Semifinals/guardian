1e00c0d15f55646ed0b6a5da82a25e6c9caea2d8
Finley Sherwood <me@itsf.in>
2023-07-16 12:55:25 +1000

    Create account repository

---
+++ Guardian.Tests.Integration/Repositories/AccountRepositoryTests.cs
@@ -0,0 +1,305 @@
+﻿using Semifinals.Guardian.Models;
+using System.Security.Principal;
+
+namespace Semifinals.Guardian.Repositories;
+
+[TestClass]
+public class AccountRepositoryTests
+{
+    [TestMethod]
+    public async Task CreateAsync_CreatesAccount()
+    {
+        // Arrange
+        string emailAddress = "user@example.com";
+        string passwordHashed = "abcd";
+        bool verified = false;
+        
+        Account account = new("test", emailAddress, passwordHashed, verified);
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Account>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(account);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.CreateItemAsync(
+                It.IsAny<Account>(),
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        Account res = await accountRepository.CreateAsync(
+            emailAddress,
+            passwordHashed);
+        
+        // Assert
+        Assert.AreEqual(account.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task CreateAsync_CreatesWithExistingId()
+    {
+        // Arrange
+        string id = "test";
+        string emailAddress = "user@example.com";
+        string passwordHashed = "abcd";
+        bool verified = false;
+
+        Account account = new(id, emailAddress, passwordHashed, verified);
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Account>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(account);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.CreateItemAsync(
+                It.IsAny<Account>(),
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+        
+        // Act
+        Account res = await accountRepository.CreateAsync(
+            emailAddress,
+            passwordHashed,
+            id);
+
+        // Assert
+        Assert.AreEqual(account.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task GetByIdAsync_GetsExistingAccount()
+    {
+        // Arrange
+        string id = "test";
+        string emailAddress = "user@example.com";
+        string passwordHashed = "abcd";
+        bool verified = false;
+
+        Account account = new(id, emailAddress, passwordHashed, verified);
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Account>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(account);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.ReadItemAsync<Account>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        Account? res = await accountRepository.GetByIdAsync(id);
+
+        // Assert
+        Assert.IsNotNull(res);
+        Assert.AreEqual(account.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task GetByIdAsync_FailsGettingNonExistentAccount()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.ReadItemAsync<Account>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ThrowsAsync(new CosmosException("", 0, 0, "", 0));
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+        
+        // Act
+        Account? res = await accountRepository.GetByIdAsync(id);
+
+        // Assert
+        Assert.IsNull(res);
+    }
+
+    [TestMethod]
+    public async Task UpdateByIdAsync_UpdatesExistingAccount()
+    {
+        // Arrange
+        string id = "test";
+        string emailAddress = "user@example.com";
+        string passwordHashed = "abcd";
+        bool verified = false;
+
+        Account account = new(id, emailAddress, passwordHashed, verified);
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Account>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(account);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.PatchItemAsync<Account>(
+                id,
+                It.IsAny<PartitionKey>(),
+                It.IsAny<IReadOnlyList<PatchOperation>>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        IEnumerable<PatchOperation> operations = new PatchOperation[]
+        {
+            PatchOperation.Add("/passwordHashed", "efgh")
+        };
+
+        // Act
+        Account? res = await accountRepository.UpdateByIdAsync(id, operations);
+
+        // Assert
+        Assert.IsNotNull(res);
+        Assert.AreEqual(account.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task UpdateByIdAsync_FailsUpdatingNonExistentAccount()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.PatchItemAsync<Account>(
+                id,
+                It.IsAny<PartitionKey>(),
+                It.IsAny<IReadOnlyList<PatchOperation>>(),
+                null,
+                default))
+            .ThrowsAsync(new CosmosException("", 0, 0, "", 0));
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        IEnumerable<PatchOperation> operations = new PatchOperation[]
+        {
+            PatchOperation.Add("/passwordHashed", "efgh")
+        };
+
+        // Act
+        Account? res = await accountRepository.UpdateByIdAsync(id, operations);
+
+        // Assert
+        Assert.IsNull(res);
+    }
+
+    [TestMethod]
+    public async Task DeleteByIdAsync_DeletesPotentiallyExistingAccount()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Account>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(value: null!);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.DeleteItemAsync<Account>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        AccountRepository accountRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        await accountRepository.DeleteByIdAsync(id);
+
+        // Assert
+
+        // Because this method doesn't care if the identity exists or not,
+        // we don't need to assert anything here. It will fail if something
+        // else is wrong with the method.
+    }
+}
--- Guardian/Repositories/AccountRepository.cs
+++ Guardian/Repositories/AccountRepository.cs
@@ -1,9 +1,133 @@
-﻿namespace Semifinals.Guardian.Repositories;
+﻿using Semifinals.Guardian.Models;
+
+namespace Semifinals.Guardian.Repositories;
 
 public interface IAccountRepository
 {
+    /// <summary>
+    /// Create a new first-party account. Provide the identity's ID if the
+    /// identity already exists from registration through an integration.
+    /// </summary>
+    /// <param name="emailAddress">The unique email address for the account</param>
+    /// <param name="passwordHashed">The account's password after being hashed</param>
+    /// <param name="id">The ID of the identity if it already exists</param>
+    /// <returns></returns>
+    Task<Account> CreateAsync(
+        string emailAddress,
+        string passwordHashed,
+        string? id = null);
+
+    /// <summary>
+    /// Get an account by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the account to fetch</param>
+    /// <returns>The associated account</returns>
+    Task<Account?> GetByIdAsync(string id);
+
+    /// <summary>
+    /// Update an account by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the account to update</param>
+    /// <param name="operations">The patch operations to perform</param>
+    /// <returns>The updated account</returns>
+    Task<Account?> UpdateByIdAsync(
+        string id,
+        IEnumerable<PatchOperation> operations);
+
+    /// <summary>
+    /// Delete an account by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the account to delete</param>
+    /// <returns>A task which resolves when the account is deleted</returns>
+    Task DeleteByIdAsync(string id);
 }
 
 public class AccountRepository : IAccountRepository
 {
+    private readonly ILogger _logger;
+    private readonly CosmosClient _cosmosClient;
+
+    private Container Container => _cosmosClient.GetContainer(
+        "identity-db",
+        "accounts");
+
+    public AccountRepository(ILogger logger, CosmosClient cosmosClient)
+    {
+        _logger = logger;
+        _cosmosClient = cosmosClient;
+    }
+    
+    public async Task<Account> CreateAsync(
+        string emailAddress,
+        string passwordHashed,
+        string? id = null)
+    {
+        id ??= ShortId.Generate(new(useSpecialCharacters: false, length: 8));
+
+        Account account = await Container.CreateItemAsync<Account>(
+            new(id, emailAddress, passwordHashed, false),
+            new(id));
+
+        _logger.LogInformation("Created new account with ID {id}", id);
+
+        return account;
+    }
+
+    public async Task<Account?> GetByIdAsync(string id)
+    {
+        Account account;
+        try
+        {
+            account = await Container.ReadItemAsync<Account>(id, new(id));
+        }
+        catch (CosmosException)
+        {
+            _logger.LogInformation(
+                "Unsuccessfully attempted to get the account with ID {id}",
+                id);
+
+            return null;
+        }
+
+        _logger.LogInformation("Fetched the account with ID {id}", id);
+        
+        return account;
+    }
+    
+    public async Task<Account?> UpdateByIdAsync(
+        string id,
+        IEnumerable<PatchOperation> operations)
+    {
+        Account account;
+        try
+        {
+            account = await Container.PatchItemAsync<Account>(
+                id,
+                new(id),
+                (IReadOnlyList<PatchOperation>)operations);
+        }
+        catch (CosmosException)
+        {
+            _logger.LogInformation(
+                "Unsuccessfully attempted to update the account with ID {id} changing the following properties: {properties}",
+                id,
+                string.Join(", ", operations.Select(o => o.Path)));
+
+            return null;
+        }
+
+        _logger.LogInformation(
+            "Updated the account with ID {id} changing the following properties: {properties}",
+            id,
+            string.Join(", ", operations.Select(o => o.Path)));
+
+        return account;
+    }
+
+    public async Task DeleteByIdAsync(string id)
+    {
+        await Container.DeleteItemAsync<Account>(id, new(id));
+
+        _logger.LogInformation("Deleted the account with ID {id}", id);
+    }
 }
