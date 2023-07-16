f2e8ace55935a2f95c996785100cd89ca20cda32
Finley Sherwood <me@itsf.in>
2023-07-16 11:40:00 +1000

    Create integration repository

---
+++ Guardian.Tests.Integration/Repositories/IntegrationRepositoryTests.cs
@@ -0,0 +1,249 @@
+﻿using Semifinals.Guardian.Models;
+
+namespace Semifinals.Guardian.Repositories;
+
+[TestClass]
+public class IntegrationRepositoryTests
+{
+    [TestMethod]
+    public async Task CreateAsync_CreatesIntegration()
+    {
+        // Arrange
+        string identityId = "identityId";
+        string platform = "platform";
+            
+        Integration integration = new("id", identityId, platform);
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Integration>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(integration);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.CreateItemAsync(
+                It.IsAny<Integration>(),
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        Integration res = await integrationRepository.CreateAsync(
+            identityId,
+            platform);
+
+        // Assert
+        Assert.AreEqual(integration.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task GetByIdAsync_GetsExistingIntegration()
+    {
+        // Arrange
+        string id = "test";
+        Integration integration = new(id, "identityId", "platform");
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Integration>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(integration);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.ReadItemAsync<Integration>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        Integration? res = await integrationRepository.GetByIdAsync(id);
+
+        // Assert
+        Assert.IsNotNull(res);
+        Assert.AreEqual(integration.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task GetByIdAsync_FailsGettingNonExistentIntegration()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.ReadItemAsync<Integration>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ThrowsAsync(new CosmosException("", 0, 0, "", 0));
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        Integration? res = await integrationRepository.GetByIdAsync(id);
+
+        // Assert
+        Assert.IsNull(res);
+    }
+
+    [TestMethod]
+    public async Task UpdateByIdAsync_UpdatesExistingIntegration()
+    {
+        // Arrange
+        string id = "test";
+        Integration integration = new(id, "identityId", "platform");
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Integration>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(integration);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.PatchItemAsync<Integration>(
+                id,
+                It.IsAny<PartitionKey>(),
+                It.IsAny<IReadOnlyList<PatchOperation>>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        IEnumerable<PatchOperation> operations = new PatchOperation[]
+        {
+            PatchOperation.Add("/identityId", "newId")
+        };
+
+        // Act
+        Integration? res = await integrationRepository.UpdateByIdAsync(id, operations);
+
+        // Assert
+        Assert.IsNotNull(res);
+        Assert.AreEqual(integration.Id, res.Id);
+    }
+
+    [TestMethod]
+    public async Task UpdateByIdAsync_FailsUpdatingNonExistentIntegration()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.PatchItemAsync<Integration>(
+                id,
+                It.IsAny<PartitionKey>(),
+                It.IsAny<IReadOnlyList<PatchOperation>>(),
+                null,
+                default))
+            .ThrowsAsync(new CosmosException("", 0, 0, "", 0));
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        IEnumerable<PatchOperation> operations = new PatchOperation[]
+        {
+            PatchOperation.Add("/identityId", "newId")
+        };
+
+        // Act
+        Integration? res = await integrationRepository.UpdateByIdAsync(id, operations);
+
+        // Assert
+        Assert.IsNull(res);
+    }
+
+    [TestMethod]
+    public async Task DeleteByIdAsync_DeletesPotentiallyExistingIntegration()
+    {
+        // Arrange
+        string id = "test";
+
+        Mock<ILogger> logger = new();
+
+        Mock<ItemResponse<Integration>> itemResponse = new();
+        itemResponse
+            .Setup(x => x.Resource)
+            .Returns(value: null!);
+
+        Mock<Container> container = new();
+        container
+            .Setup(x => x.DeleteItemAsync<Integration>(
+                id,
+                It.IsAny<PartitionKey>(),
+                null,
+                default))
+            .ReturnsAsync(itemResponse.Object);
+
+        Mock<CosmosClient> cosmosClient = new();
+        cosmosClient
+            .Setup(x => x.GetContainer(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(container.Object);
+
+        IntegrationRepository integrationRepository = new(
+            logger.Object,
+            cosmosClient.Object);
+
+        // Act
+        await integrationRepository.DeleteByIdAsync(id);
+
+        // Assert
+
+        // Because this method doesn't care if the integration exists or not,
+        // we don't need to assert anything here. It will fail if something
+        // else is wrong with the method.
+    }
+}
--- Guardian/Repositories/IntegrationRepository.cs
+++ Guardian/Repositories/IntegrationRepository.cs
@@ -1,9 +1,125 @@
-﻿namespace Semifinals.Guardian.Repositories;
+﻿using Semifinals.Guardian.Models;
+
+namespace Semifinals.Guardian.Repositories;
 
 public interface IIntegrationRepository
 {
+    /// <summary>
+    /// Create a new integration.
+    /// </summary>
+    /// <param name="identityId">The unique ID on the given platform</param>
+    /// <param name="platform">The platform the integration comes from</param>
+    /// <returns>The newly created integration</returns>
+    Task<Integration> CreateAsync(string identityId, string platform);
+
+    /// <summary>
+    /// Get an integration by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the integration to fetch</param>
+    /// <returns>The requested integration</returns>
+    Task<Integration?> GetByIdAsync(string id);
+
+    /// <summary>
+    /// Update a given integration by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the integration to update</param>
+    /// <param name="operations">The patch operations to perform</param>
+    /// <returns>The updated integration</returns>
+    Task<Integration?> UpdateByIdAsync(
+        string id,
+        IEnumerable<PatchOperation> operations);
+
+    /// <summary>
+    /// Delete an integration by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the integration to delete</param>
+    /// <returns>A task which will resolve once the integration is deleted</returns>
+    Task DeleteByIdAsync(string id);
 }
 
 public class IntegrationRepository : IIntegrationRepository
 {
+    private readonly ILogger _logger;
+    private readonly CosmosClient _cosmosClient;
+
+    private Container Container => _cosmosClient.GetContainer(
+        "identity-db",
+        "integrations");
+
+    public IntegrationRepository(ILogger logger, CosmosClient cosmosClient)
+    {
+        _logger = logger;
+        _cosmosClient = cosmosClient;
+    }
+    
+    public async Task<Integration> CreateAsync(string identityId, string platform)
+    {
+        string id = ShortId.Generate(new(useSpecialCharacters: false, length: 8));
+        
+        Integration integration = await Container.CreateItemAsync<Integration>(
+            new(id, identityId, platform),
+            new(id));
+
+        _logger.LogInformation("Created new integration with ID {id}", id);
+        
+        return integration;
+    }
+    
+    public async Task<Integration?> GetByIdAsync(string id)
+    {
+        Integration integration;
+        try
+        {
+            integration = await Container.ReadItemAsync<Integration>(id, new(id));
+        }
+        catch (CosmosException)
+        {
+            _logger.LogInformation(
+                "Unsuccessfully attempted to get the integration with ID {id}",
+                id);
+
+            return null;
+        }
+
+        _logger.LogInformation("Fetched the integration with ID {id}", id);
+
+        return integration;
+    }
+    
+    public async Task<Integration?> UpdateByIdAsync(
+        string id,
+        IEnumerable<PatchOperation> operations)
+    {
+        Integration integration;
+        try
+        {
+            integration = await Container.PatchItemAsync<Integration>(
+                id,
+                new(id),
+                (IReadOnlyList<PatchOperation>)operations);
+        }
+        catch (CosmosException)
+        {
+            _logger.LogInformation(
+                "Unsuccessfully attempted to update the integration with ID {id} changing the following properties: {properties}",
+                id,
+                string.Join(", ", operations.Select(o => o.Path)));
+
+            return null;
+        }
+
+        _logger.LogInformation(
+            "Updated the integration with ID {id} changing the following properties: {properties}",
+            id,
+            string.Join(", ", operations.Select(o => o.Path)));
+
+        return integration;
+    }
+
+    public async Task DeleteByIdAsync(string id)
+    {
+        await Container.DeleteItemAsync<Integration>(id, new(id));
+
+        _logger.LogInformation("Deleted the integration with ID {id}", id);
+    }    
 }
