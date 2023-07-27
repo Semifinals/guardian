7ed297d71e1145f5b8dc545c869f206782b4f0bf
Finley Sherwood <me@itsf.in>
2023-07-27 15:04:51 +1000

    Create association service

--- Guardian.Tests.Integration/Services/AssociationServiceTests.cs
+++ Guardian.Tests.Integration/Services/AssociationServiceTests.cs
@@ -1,6 +1,304 @@
-﻿namespace Semifinals.Guardian.Services;
+﻿using Semifinals.Guardian.Models;
+using Semifinals.Guardian.Repositories;
+using Semifinals.Guardian.Utils;
+
+namespace Semifinals.Guardian.Services;
 
 [TestClass]
 public class AssosciationServiceTests
 {
+    [TestMethod]
+    public async Task AddAccountAsync_SuccessfullyAddsAccount()
+    {
+        // Arrange
+        string id = "id";
+        string emailAddress = "user@example.com";
+        string password = "password";
+        string passwordHashed = Crypto.Hash(password);
+        
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        accountRepository
+            .Setup(x => x.CreateAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+            .ReturnsAsync(new Account(id, emailAddress, passwordHashed, false));
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Account? account = await associationService.AddAccountAsync(
+            id,
+            emailAddress,
+            password);
+
+        // Assert
+        Assert.IsNotNull(account);
+        Assert.AreEqual(id, account.Id);
+    }
+
+    [TestMethod]
+    public async Task AddAccountAsync_FailsOnNonExistentIdentity()
+    {
+        // Arrange
+        string id = "id";
+        string emailAddress = "user@example.com";
+        string password = "password";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(value: null);
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Account? account = await associationService.AddAccountAsync(
+            id,
+            emailAddress,
+            password);
+
+        // Assert
+        Assert.IsNull(account);
+    }
+
+    [TestMethod]
+    public async Task AddAccountAsync_FailsOnExistingEmailAddress()
+    {
+        // Arrange
+        string id = "id";
+        string emailAddress = "user@example.com";
+        string password = "password";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        accountRepository
+            .Setup(x => x.CreateAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                null))
+            .ReturnsAsync(value: null);
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Account? account = await associationService.AddAccountAsync(
+            id,
+            emailAddress,
+            password);
+
+        // Assert
+        Assert.IsNull(account);        
+    }
+
+    [TestMethod]
+    public async Task AddIntegrationAsync_SuccessfullyAddsIntegration()
+    {
+        // Arrange
+        string id = "id";
+        string platform = "platform";
+        string userId = "userId";
+        string compositeId = Integration.GetCompositeId(platform, userId);
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        integrationRepository
+            .Setup(x => x.CreateAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+            .ReturnsAsync(new Integration(id, platform, userId));
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Integration? integration = await associationService.AddIntegrationAsync(
+            id,
+            platform,
+            userId);
+
+        // Assert
+        Assert.IsNotNull(integration);
+        Assert.AreEqual(compositeId, integration.Id);
+    }
+
+    [TestMethod]
+    public async Task AddIntegrationAsync_FailsOnNonExistentIdentity()
+    {
+        // Arrange
+        string id = "id";
+        string platform = "platform";
+        string userId = "userId";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Integration? integration = await associationService.AddIntegrationAsync(
+            id,
+            platform,
+            userId);
+
+        // Assert
+        Assert.IsNull(integration);
+    }
+
+    [TestMethod]
+    public async Task AddIntegrationAsync_FailsOnExistingIntegration()
+    {
+        // Arrange
+        string id = "id";
+        string platform = "platform";
+        string userId = "userId";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        integrationRepository
+            .Setup(x => x.CreateAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+            .ReturnsAsync(value: null);
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Integration? integration = await associationService.AddIntegrationAsync(
+            id,
+            platform,
+            userId);
+
+        // Assert
+        Assert.IsNull(integration);
+    }
+
+    [TestMethod]
+    public async Task RemoveIntegrationAsync_SuccessfullyRemovesIntegration()
+    {
+        // Arrange
+        string id = "id";
+        string platform = "platform";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(new Identity(id));
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Identity? identity = await associationService.RemoveIntegrationAsync(
+            id,
+            platform);
+
+        // Assert
+
+        // Because this operation doesn't care if the integration exists or not,
+        // we don't need to assert anything here. It will fail if something
+        // else is wrong with the method.
+    }
+
+    [TestMethod]
+    public async Task RemoveIntegrationAsync_FailsOnNonExistentIdentity()
+    {
+        // Arrange
+        string id = "id";
+        string platform = "platform";
+
+        Mock<ILogger> logger = new();
+        Mock<IAccountRepository> accountRepository = new();
+        Mock<IIdentityRepository> identityRepository = new();
+        Mock<IIntegrationRepository> integrationRepository = new();
+
+        identityRepository
+            .Setup(x => x.GetByIdAsync(id))
+            .ReturnsAsync(value: null);
+
+        AssociationService associationService = new(
+            logger.Object,
+            accountRepository.Object,
+            identityRepository.Object,
+            integrationRepository.Object);
+
+        // Act
+        Identity? identity = await associationService.RemoveIntegrationAsync(
+            id,
+            platform);
+
+        // Assert
+        Assert.IsNull(identity);
+    }
 }
--- Guardian/Services/AssociationService.cs
+++ Guardian/Services/AssociationService.cs
@@ -1,5 +1,6 @@
 ﻿using Semifinals.Guardian.Models;
 using Semifinals.Guardian.Repositories;
+using Semifinals.Guardian.Utils;
 
 namespace Semifinals.Guardian.Services;
 
@@ -22,38 +23,39 @@ public interface IAssociationService
     /// Add a third-party integration to an account.
     /// </summary>
     /// <param name="id">The user's identity ID</param>
-    /// <param name="platformId">The user's unique ID on the other platform</param>
     /// <param name="platform">The name of the platform registering through</param>
+    /// <param name="userId">The user's unique ID on the other platform</param>
     /// <returns>The newly associated integration</returns>
     Task<Integration?> AddIntegrationAsync(
         string id,
-        string platformId,
-        string platform);
+        string platform,
+        string userId);
 
     /// <summary>
     /// Remove a third-party integration from an account.
     /// </summary>
     /// <param name="id">The user's identity ID</param>
-    /// <param name="platformId">The user's unique ID on the other platform</param>
     /// <param name="platform">The name of the platform registering through</param>
-    /// <returns>The integration that's association was removed</returns>
-    Task<Integration?> RemoveIntegrationAsync(
+    /// <returns>The resulting identity after the integration was removed</returns>
+    Task<Identity?> RemoveIntegrationAsync(
         string id,
-        string platformId,
         string platform);
 }
 
 public class AssociationService : IAssociationService
 {
+    private readonly ILogger _logger;
     private readonly IAccountRepository _accountRepository;
     private readonly IIdentityRepository _identityRepository;
     private readonly IIntegrationRepository _integrationRepository;
 
     public AssociationService(
+        ILogger logger,
         IAccountRepository accountRepository,
         IIdentityRepository identityRepository,
         IIntegrationRepository integrationRepository)
     {
+        _logger = logger;
         _accountRepository = accountRepository;
         _identityRepository = identityRepository;
         _integrationRepository = integrationRepository;
@@ -64,22 +66,118 @@ public class AssociationService : IAssociationService
         string emailAddress,
         string password)
     {
-        throw new NotImplementedException();
+        // Check the identity exists
+        Identity? identity = await _identityRepository.GetByIdAsync(id);
+
+        if (identity is null)
+        {
+            _logger.LogCritical(
+                "Failed to add account {emailAddress} to non-existent identity {id}",
+                emailAddress,
+                id);
+
+            return null;
+        }
+
+        // Create the account
+        string passwordHashed = Crypto.Hash(password);
+        Account? account = await _accountRepository.CreateAsync(
+            emailAddress,
+            passwordHashed,
+            id);
+
+        if (account is null)
+        {
+            _logger.LogInformation(
+                "Failed to add account {emailAddress} to identity {id} since it is already in use",
+                emailAddress,
+                id);
+
+            return null;
+        }
+
+        // Return the newly created account
+        _logger.LogInformation(
+            "Successfully added account {emailAddress} to identity {id}",
+            emailAddress,
+            id);
+
+        return account;
     }
 
     public async Task<Integration?> AddIntegrationAsync(
         string id,
-        string platformId,
-        string platform)
+        string platform,
+        string userId)
     {
-        throw new NotImplementedException();
-    }
+        // Check the identity exists
+        Identity? identity = await _identityRepository.GetByIdAsync(id);
 
-    public async Task<Integration?> RemoveIntegrationAsync(
+        if (identity is null)
+        {
+            _logger.LogCritical(
+                "Failed to add integration {userId} ({platform}) to non-existent identity {id}",
+                userId,
+                platform,
+                id);
+
+            return null;
+        }
+
+        // Create the integration
+        Integration? integration = await _integrationRepository.CreateAsync(
+            id,
+            platform,
+            userId);
+
+        if (integration is null)
+        {
+            _logger.LogInformation(
+                "Failed to add integration {userId} ({platform}) to identity {id} since it is already in use",
+                userId,
+                platform,
+                id);
+
+            return null;
+        }
+
+        // Link integration to account
+        await _identityRepository.UpdateByIdAsync(identity.Id, new PatchOperation[]
+        {
+            PatchOperation.Add($"/integrations/{platform}", userId)
+        });
+
+        // Return the newly created integration
+        _logger.LogInformation(
+            "Successfully added integration {userId} ({platform}) to identity {id}",
+            userId,
+            platform,
+            id);
+
+        return integration;
+    }
+    
+    public async Task<Identity?> RemoveIntegrationAsync(
         string id,
-        string platformId,
         string platform)
     {
-        throw new NotImplementedException();
+        Identity? identity = await _identityRepository.UpdateByIdAsync(id, new PatchOperation[]
+        {
+            PatchOperation.Remove($"/integrations/{platform}")
+        });
+
+        if (identity is null)
+        {
+            _logger.LogInformation(
+                "Failed to remove {platform} integration from identity {id}",
+                platform,
+                id);
+            
+            return null;
+        }
+
+        await _integrationRepository.DeleteByIdAsync(id);
+
+        return identity;
     }
 }
